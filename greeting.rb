05f115ce60a2ebb3ba6f15a4dd1831e8dcac7e55
khan <gooduraiz_khan@yahoo.com>
2017-06-12 02:22:47 +0000

    checkpoint 5 full

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+    use = ARGV.shift
+     ARGV.each do |arg|
+        p "#{use} #{arg}"
+    end
---
+++ hello_world.rb
@@ -0,0 +1,9 @@
+ def hello_world
+ #
+   ARGV.each do |arg|
+     puts "Hello, #{arg}!"
+   end
+    
+ end
+
+hello_world
